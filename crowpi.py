821d0e6458f484db614e1c2c0d81920704cc1ee8
Ari <144230403+Arimuon@users.noreply.github.com>
2024-02-14 13:47:26 +0000

    Create crowpi.py

---
+++ crowpi.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+import time
+
+buzzer_pin = 18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(buzzer_pin, GPIO.OUT)
+
+# Make buzzer sound 
+GPIO.output(buzzer_pin, GPIO.HIGH)
+time.sleep(0.5)
+# Stop buzzer sound
+GPIO.output(buzzer_pin, GPIO.LOW)
+
+GPIO.output(buzzer_pin, GPIO.HIGH)
+time.sleep(0.5)
+# Stop buzzer sound
+GPIO.output(buzzer_pin, GPIO.LOW)
+
+GPIO.output(buzzer_pin, GPIO.HIGH)
+time.sleep(0.5)
+# Stop buzzer sound
+GPIO.output(buzzer_pin, GPIO.LOW)
+
+GPIO.output(buzzer_pin, GPIO.HIGH)
+GPIO.output(buzzer_pin, GPIO.HIGH)
+GPIO.output(buzzer_pin, GPIO.HIGH)
+time.sleep(0.5)
+# Stop buzzer sound
+GPIO.output(buzzer_pin, GPIO.LOW)
+
+GPIO.cleanup()
