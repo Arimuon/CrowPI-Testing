65630362568bca696e0b1241dccb02adeacf8c5e
Ari <144230403+Arimuon@users.noreply.github.com>
2024-02-14 13:51:56 +0000

    Update crowpi.py

--- crowpi.py
+++ crowpi.py
@@ -2,31 +2,34 @@ import RPi.GPIO as GPIO
 import time
 
 buzzer_pin = 18
+button_pin = 26
+buzzer_pin = 18
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(buzzer_pin, GPIO.OUT)
 
-# Make buzzer sound 
-GPIO.output(buzzer_pin, GPIO.HIGH)
-time.sleep(0.5)
-# Stop buzzer sound
-GPIO.output(buzzer_pin, GPIO.LOW)
+# set board mode to GPIO.BOARD
+GPIO.setmode(GPIO.BCM)
 
-GPIO.output(buzzer_pin, GPIO.HIGH)
-time.sleep(0.5)
-# Stop buzzer sound
-GPIO.output(buzzer_pin, GPIO.LOW)
+# setup button pin asBu input and buzzer pin as output
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(buzzer_pin, GPIO.OUT)
 
-GPIO.output(buzzer_pin, GPIO.HIGH)
-time.sleep(0.5)
-# Stop buzzer sound
-GPIO.output(buzzer_pin, GPIO.LOW)
-
-GPIO.output(buzzer_pin, GPIO.HIGH)
-GPIO.output(buzzer_pin, GPIO.HIGH)
-GPIO.output(buzzer_pin, GPIO.HIGH)
-time.sleep(0.5)
-# Stop buzzer sound
-GPIO.output(buzzer_pin, GPIO.LOW)
-
-GPIO.cleanup()
+try:
+    while True:
+        # check if button pressed
+        if(GPIO.input(button_pin) == 0):
+            # set buzzer on
+            GPIO.output(buzzer_pin, GPIO.HIGH)
+            GPIO.output(buzzer_pin, GPIO.LOW)
+            time.sleep(0.5)
+            GPIO.output(buzzer_pin, GPIO.LOW)
+            time.sleep(0.5)
+            GPIO.output(buzzer_pin, GPIO.LOW)
+            time.sleep(0.5)
+            GPIO.output(buzzer_pin, GPIO.HIGH)
+        else:
+            # it's not pressed, set button off
+            GPIO.output(buzzer_pin, GPIO.LOW)
+except KeyboardInterrupt:
+    GPIO.cleanup()
